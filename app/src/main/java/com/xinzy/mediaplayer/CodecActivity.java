2af74fcf4ec9da0623f2ee7ab1da2ec58067739d
Xinzy <shaozengy@gmail.com>
2016-05-27 18:37:23 +0800

    初始化提交

--- app/src/main/java/com/xinzy/mediaplayer/CodecActivity.java
+++ app/src/main/java/com/xinzy/mediaplayer/CodecActivity.java
@@ -2,12 +2,19 @@ package com.xinzy.mediaplayer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+
+import java.io.IOException;
 
 public class CodecActivity extends AppCompatActivity implements SurfaceHolder.Callback
 {
@@ -37,7 +44,7 @@ public class CodecActivity extends AppCompatActivity implements SurfaceHolder.Ca
 
 	public void start(View v)
 	{
-
+		new CodecThread(mHolder.getSurface()).start();
 	}
 
 	@Override
@@ -62,6 +69,48 @@ public class CodecActivity extends AppCompatActivity implements SurfaceHolder.Ca
 
 	class CodecThread extends Thread
 	{
+		private MediaCodec mMediaCodec;
+		private MediaExtractor mExtractor;
 
+		private Surface mSurface;
+		private String filePath;
+
+		public CodecThread(Surface surface)
+		{
+			mSurface = surface;
+			filePath = Environment.getExternalStorageDirectory() + "/Movies/jinganglang01.mp4";
+		}
+
+		@Override
+		public void run()
+		{
+			mExtractor = new MediaExtractor();
+			try
+			{
+				mExtractor.setDataSource(filePath);
+				final int nTracks = mExtractor.getTrackCount();
+				Log.d(TAG, "track count = " + nTracks);
+
+				for (int i = 0; i < nTracks; i++)
+				{
+					MediaFormat format = mExtractor.getTrackFormat(i);
+					String mine = format.getString(MediaFormat.KEY_MIME);
+					Log.d(TAG, "mine type = " + mine);
+
+					if (mine.startsWith("video/"))
+					{
+						mExtractor.selectTrack(i);
+						mMediaCodec = MediaCodec.createDecoderByType(mine);
+						mMediaCodec.configure(format, mSurface, null, 0);
+					}
+				}
+
+				if (mMediaCodec == null) return ;
+				mMediaCodec.start();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
